f78b9f5516eeebe76e2cefe807fdbed105ec6849
Jack King <email@qiaohong.org>
2022-04-22 03:15:00 +0800

    feat: add circular buffer

---
+++ buffer/circular.go
@@ -0,0 +1,97 @@
+package buffer
+
+import (
+	"errors"
+	"sync"
+)
+
+var (
+	// ErrCircularBufferEmpty is returned when the buffer is empty.
+	ErrCircularBufferEmpty = errors.New("circular buffer is empty")
+	// ErrCircularBufferFull is returned when the buffer is full.
+	ErrCircularBufferFull = errors.New("circular buffer is full")
+	// ErrTooManyDataToWrite is returned when the buffer available is less than ready to write.
+	ErrTooManyDataToWrite = errors.New("circular buffer too many data to write")
+)
+
+// CircularBuffer is a thread-safe circular buffer.
+type CircularBuffer struct {
+	size   int
+	buf    []byte
+	r, w   int // next position to read & write
+	length int // current used length
+	mu     sync.Mutex
+}
+
+func NewCircularBuffer(size int) *CircularBuffer {
+	return &CircularBuffer{
+		size: size,
+		buf:  Get(size),
+	}
+}
+
+func (circular *CircularBuffer) Len() int {
+	return circular.length
+}
+
+func (circular *CircularBuffer) Read(data []byte) (n int, err error) {
+	circular.mu.Lock()
+	defer circular.mu.Unlock()
+
+	if circular.length == 0 {
+		return 0, ErrCircularBufferEmpty
+	}
+
+	if circular.r < circular.w {
+		n = copy(data, circular.buf[circular.r:circular.w])
+		circular.r = circular.r + n
+	} else {
+		n = copy(data, circular.buf[circular.r:])
+		n += copy(data[n:], circular.buf[:circular.w])
+		circular.r = circular.r + n - circular.size
+	}
+
+	circular.length -= n
+	return n, nil
+}
+
+func (circular *CircularBuffer) Write(data []byte) (n int, err error) {
+	if len(data) == 0 {
+		return 0, nil
+	}
+
+	circular.mu.Lock()
+	defer circular.mu.Unlock()
+
+	if circular.length >= circular.size {
+		return 0, ErrCircularBufferFull
+	}
+
+	avail := circular.size - circular.length
+	if avail < len(data) {
+		return 0, ErrTooManyDataToWrite
+	}
+
+	if circular.r < circular.w {
+		if circular.size-circular.w >= len(data) {
+			n = copy(circular.buf[circular.w:], data)
+			circular.w = circular.w + n
+		} else {
+			n = copy(circular.buf[circular.w:], data)
+			n += copy(circular.buf[:circular.r], data[n:])
+			circular.w = circular.w + n - circular.size
+		}
+	} else {
+		n = copy(circular.buf[circular.w:], data)
+		circular.w = circular.w + n
+	}
+
+	circular.length += n
+	return n, nil
+}
+
+// Close recycle buffer.
+func (circular *CircularBuffer) Close() error {
+	Put(circular.buf)
+	return nil
+}
---
+++ buffer/circular_test.go
@@ -0,0 +1,36 @@
+package buffer
+
+import "testing"
+
+func TestCircularBuffer_Write(t *testing.T) {
+	circularBuffer := NewCircularBuffer(20)
+
+	content := []byte("Hello World")
+
+	n, err := circularBuffer.Write(content)
+	if err!=nil {
+		t.Errorf("Error writing to circularBuffer: %s", err)
+	}
+	if n!=len(content) {
+		t.Errorf("Expected to write %d bytes, but wrote %d", len(content), n)
+	}
+	if circularBuffer.Len()!=len(content) {
+		t.Errorf("Expected circularBuffer length to be %d, but was %d", len(content), circularBuffer.Len())
+	}
+
+	readBuffer := Get(20)
+	n, err = circularBuffer.Read(readBuffer)
+	if err !=nil {
+		t.Errorf("Error reading from circularBuffer: %s", err)
+	}
+	if n!=len(content) {
+		t.Errorf("Expected to read %d bytes, but read %d", len(content), n)
+	}
+	if string(readBuffer[:n])!=string(content) {
+		t.Errorf("Expected to read %s, but read %s", string(content), string(readBuffer))
+	}
+
+	if circularBuffer.Len()!=0 {
+		t.Errorf("Expected circularBuffer length to be 0, but was %d", circularBuffer.Len())
+	}
+}
